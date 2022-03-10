e80771475a30638e9c0b618edd11d54f8d393a1a
ginabeki <ginabeki3@gmail.com>
2022-03-10 06:35:58 -0800

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 /**
- * main -entry point
- * description: use printf
- * return:0
- */
+ *  * main - Print a string
+ *   * Description: use printf
+ *    * Return: 0
+ *     */
 
 int main(void)
 {
-	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+		printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
 }
