3e442ab274e08b1ba6eb7e50c24b587f8d7c518c
ginabeki <ginabeki3@gmail.com>
2022-03-10 06:42:11 -0800

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- *  * main - Print a string
- *   * Description: use printf
- *    * Return: 0
- *     */
+ * main - Print a string
+ * Description: use printf
+ * Return: 0
+ */
 
 int main(void)
 {
