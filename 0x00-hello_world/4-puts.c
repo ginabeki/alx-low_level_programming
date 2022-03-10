ff3ff86173070fa29be1d9647a473d816a22ae7d
ginabeki <ginabeki3@gmail.com>
2022-03-10 06:09:04 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 
 /**
  * main - Entry point
- * Description: prints "Programming is like building a multilingual puzzle\n"
+ * Description: prints ""Programming is like building a multilingual puzzle\n"
  * Return: 0
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
