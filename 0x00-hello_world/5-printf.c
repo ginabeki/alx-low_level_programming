cc90e49611f3d3b94534fbad87cd33223d1905ed
ginabeki <ginabeki3@gmail.com>
2022-03-10 06:22:34 -0800

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ * description: use printf
+ * return: o
+ */
+
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
