30609a87566d791cd48317fd4e54b987015222ff
ginabeki <ginabeki3@gmail.com>
2022-04-07 18:46:22 +0300

    Update header files

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -19,9 +19,4 @@ int *array_range(int min, int max);
 /* _realloc - reallocates a memory block using malloc and free.*/
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-int is_digit(char *s);
-int _strlen(char *s);
-void errors(void);
-int main(int argc, char *argv[]);
-
 #endif /* MAIN_H */
